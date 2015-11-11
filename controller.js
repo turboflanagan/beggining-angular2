a93e8f73c92eefbfa4c5fbf4625599da52e8f525
turboflanagan <turboflanagan@gmail.com>
2015-11-11 10:18:36 -0500

    init commit

---
+++ controller.js
@@ -0,0 +1,37 @@
+
+
+
+
+var myApp=angular.module('myApp', [])
+myApp.controller('myController', function($scope){
+	$scope.items = [
+	{
+		name: "Freddy", 
+		desc: "Made a cool weather widget"
+	},
+	{
+		name: "Chance",
+		desc: "Loves Bootstrap"
+	},
+	{
+		name: "Stuart",
+		desc: "dRove a long way to get here today"
+	},
+	{
+		name: "Will",
+		desc: "the tall one"
+	},
+	{
+		name: "Rob",
+		desc: "the teach."
+	}
+
+	];
+});
+
+
+
+
+
+
+
